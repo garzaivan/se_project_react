df11f6de3dc6b090083d15ca1232a51135028128
ivangarza <garzaivan2709@yahoo.com>
2025-09-17 11:43:21 -0500

    Update Project WTWR

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__text">Develped by Ivan Garza &copy;</p>
+      <p className="footer__text">Developed by Ivan Garza &copy;</p>
       <div>
         <p className="footer__text">2025</p>
       </div>
